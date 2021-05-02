d3dd087ddffa0db8d89cc14cfee4e562ec6977c7
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-01 19:24:03 -0700

    commit tree 91f1945c379f597c19dd6f5977f637b7a6fc4e50

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,10 +3,23 @@ import React from 'react'
 class Filter extends React.Component {
     render() {
         return (
-        <form onChange={()=>{}}>
-            <label>Greased<input type="radio" name="greased" value="greased" className="radiobox" /></label>
-            <label>Ungreased<input type="radio" name="greased" value="ungreased" className="radiobox" /></label>
-            <label>All<input type="radio" name="greased" value="all" className="radiobox" /></label>
+        <form className="filterWrapper" onChange={(e)=>{this.props.setFilter(e.target.value)}}>
+            <label className="ui label" >Greased<input 
+            type="radio" 
+            name="greased" 
+            value="greased" 
+            // {[this.props.filter === this.value] ? 'checked' : null}
+            className="ui radio checkbox" /></label>
+            <label className="ui label" >Ungreased<input 
+            type="radio" 
+            name="greased" 
+            value="ungreased" 
+            className="ui radio checkbox" /></label>
+            <label className="ui label" >All<input 
+            type="radio" 
+            name="greased" 
+            value="all" 
+            className="ui radio checkbox" /></label>
         </form>)
     }
 }
--- src/components/PigIndex.js
+++ src/components/PigIndex.js
@@ -3,14 +3,40 @@ import PigTile from './PigTile'
 
 class PigIndex extends React.Component {
     
+    sort= (unsortedArray) => {
+        let unsortedHogs = [...unsortedArray]
+        if (this.props.sort === 'name') {
+            return unsortedHogs.sort(function(a, b) {
+                if (a.name < b.name) {
+                    return -1
+                }
+                if (a.name > b.name) {
+                    return 1
+                }
+                return 0
+            })
+        } else if (this.props.sort === 'weight') {
+            return unsortedHogs.sort(function(a, b) {return a.weight - b.weight})
+        } else {
+            return unsortedHogs
+        }
+    }
+    
+    filter = (unfilteredArray) => {
+        if (this.props.filter === 'greased') {return unfilteredArray.filter(hog => hog.greased)}
+        else if (this.props.filter === 'ungreased') {return unfilteredArray.filter(hog => !hog.greased)}
+        else {return unfilteredArray}
+    }
 
-    renderDetails = () => {
-        return this.props.hogs.map(hog => {return <PigTile hog={hog}/>})
+    renderDetails = (hogArray) => {
+        let filteredAndSortedHogs = this.filter(this.sort(hogArray))
+        
+        return filteredAndSortedHogs.map(hog => {return <PigTile hog={hog}/>})
     }
 
     render() {
         console.log(this.props.hogs)
-        return <div>{this.renderDetails()}</div>
+        return <div className='indexWrapper'>{this.renderDetails(this.props.hogs)}</div>
     }
 }   
 
